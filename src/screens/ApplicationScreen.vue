62ab3468c19078cb4e22a4496b606273d88e9150
reosfire <2.com9761721@gmail.com>
2024-03-03 17:57:09 +0300

    removed generated shit

---
+++ src/screens/ApplicationScreen.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/auth">Auth</RouterLink>
+        </nav>
+    </header>
+</template>
+
+<style scoped></style>
---
+++ src/screens/AuthorizationScreen.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const str = ref("aaaaa")
+
+const update = () => {
+    str.value += "a"
+}
+
+</script>
+
+<template>
+    <input v-model="str"/>
+    <div @click="update">{{ str }}</div>
+</template>
+
+<style scoped>
+div {
+    cursor: pointer;
+    user-select: none;
+}
+</style>
